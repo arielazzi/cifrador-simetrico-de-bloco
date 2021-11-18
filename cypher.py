7aa840c2c6e2b34fca548b30297a4992d62a02c9
Ariel Azzi <arielsazzi@gmail.com>
2021-11-18 12:07:24 -0300

    add key scheduling

--- cypher.py
+++ cypher.py
@@ -1,5 +1,12 @@
-def key_schedule(user_provided_key):
-    print('key_schedule')
+def key_schedule(user_provided_key, permuted_choice):
+    keys = []
+    print(binary_to_string(user_provided_key))
+    bin_user_provided_key = binary_to_string(user_provided_key)
+    keys.append(mix_blocks(bin_user_provided_key, permuted_choice))
+    keys.append(mix_blocks(keys[0], permuted_choice))
+    keys.append(mix_blocks(keys[1], permuted_choice))
+    
+    return keys
 
 
 def encrypt(keys, data):
@@ -8,3 +15,21 @@ def encrypt(keys, data):
 
 def decrypt(keys, data):
     print('decrypt')
+
+
+def binary_to_string(binary):
+    binary_text = ''
+    for letter in range(0, len(binary)):
+        binary_text += format(binary[letter], "b").zfill(8)
+    return binary_text
+
+
+def mix_blocks(bin_user_provided_key, permuted_choice):
+    blocks = ''
+    for i in range(0, len(bin_user_provided_key)):
+        blocks += bin_user_provided_key[permuted_choice[i]]
+    block1 = blocks[0:8]
+    block2 = blocks[8:16]
+    block3 = blocks[16:24]
+    block4 = blocks[24:32]
+    return block3 + block1 + block4 + block2
--- main.py
+++ main.py
@@ -11,5 +11,8 @@ user_provided_key = ''
 while len(user_provided_key.encode('utf-8')) != 4:
     user_provided_key = input("Informe o valor da chave (4 bytes): ")
 
-keys = cypher.key_schedule(user_provided_key.encode('utf-8'))
+pc1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+
+keys = cypher.key_schedule(user_provided_key.encode('utf-8'), pc1)
+print(keys)
 cypher.encrypt(keys, file.read())
